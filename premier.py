a25539f355295068b1dd9717a808468ea51993f2
DUBIPRINCI <77845981+DUBIPRINCI@users.noreply.github.com>
2024-09-09 17:27:19 +0200

    Add files via upload

--- premier.py
+++ premier.py
@@ -1,4 +1,5 @@
 from random import*
+import math
 
 a = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#@&%'
 def hacker():
@@ -10,7 +11,6 @@ def hacker():
         print(ligne)
         ligne = ''
 
-# hacker()
 
 caracteres = ['abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '1234567890', '&#@*+']
 def mdpgenerator():
@@ -22,4 +22,50 @@ def mdpgenerator():
         mdp += liste[randint(0,len(liste)-1)]
     return mdp
 
+
+def posneg():
+    n = int(input("nombre : "))
+    if n > 0 :
+        return "le nombre est positif"
+    elif n < 0 :
+        return "le nombre est négatif"
+    else : 
+        return "le nombre est nul"
+
+def imc():
+    taille = int(input('taille : '))/100
+    poids = int(input('poids : '))
+    imc = poids / taille**2
+    if imc > 25 :
+        return "obésité"
+    elif imc < 18.5 :
+        return "maigre"
+    else :
+        return "bonne santé"
+
+def celsiusToKelvin(deg, isCelsius):
+    if isCelsius :
+        return deg + 273,15
+    else :
+        return deg - 273,15
+
+def isChauffard():
+    km = int(input("distance en m : "))/1000
+    h = int(input("temps en s : "))/3600
+    v = km/h
+    if v > 50 :
+        return "chauffard !"
+    else :
+        return "conduite légale"
+
+def heureIsValide(h):
+    for i in range(len(h)):
+        if h[i] == h
+
 # print(mdpgenerator())
+# hacker()
+# print(posneg())
+# print(imc())
+# print(celsiusToKelvin(14, True))
+# print(isChauffard())
+print(heureIsValide({'h':17, 'm':84, 's':32}))
