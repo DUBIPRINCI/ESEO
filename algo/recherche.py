2478d4111b010a381c4bd328d0b2a2f0febd645d
DUBIPRINCI <dubiprinci@gmail.com>
2024-09-11 11:44:35 +0200

    tri bulles et selection

--- algo/recherche.py
+++ algo/recherche.py
@@ -11,6 +11,31 @@ def dichotomie(n, liste):
             fin = m - 1
     return False
 
+# print(dichotomie(13, [1,2,4,6,9,12,15,20,21,22,23,25,30]))
+
+def triBulles(liste):
+    nbIterations = 0
+    for i in range(len(liste)):
+        for j in range(i, len(liste)):
+            if liste[j] < liste[i]:
+                liste[i],liste[j] = liste[j],liste[i]
+                print(liste)
+                nbIterations += 1
+    return "nombre itérations : " + str(nbIterations)
+
+print(triBulles([12,52,63,97,54,29,91,24,38,46]))
+
+def triSelection(liste):
+    nbIterations = 0
+    for i in range(len(liste)):
+        min = i
+        for j in range(i+1, len(liste)):
+            if liste[min] > liste[j]:
+                min = j
+        liste[i],liste[min] = liste[min],liste[i]
+        print(liste)
+        nbIterations += 1
+    return "nombre itérations : " + str(nbIterations)
 
 
-print(dichotomie(13, [1,2,4,6,9,12,15,20,21,22,23,25,30]))
+print(triSelection([12,52,63,97,54,29,91,24,38,46]))
